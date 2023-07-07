c9147d1d64348415bc5436c04cbf985ca9fb361a
walteralleyz <walterdasilvasantos@gmail.com>
2023-07-06 21:08:46 -0300

    Initial Commit

---
+++ Chess/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by MacHome on 06/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
